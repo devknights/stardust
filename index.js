2c5c33695077697cf4ba7c7d99a3dfe0cd1584d6
jwhyte88 <jwhyte@opentable.com>
2016-07-14 16:57:20 +0100

    Updated to use jade view

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(process.env.PORT || 3000, function () {
